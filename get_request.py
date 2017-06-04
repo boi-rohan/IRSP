8ba3a1989f75d816fe2abae0080bf9f2324f0ff3
boi-rohan <rohankaushik@hotmail.com>
2017-06-05 00:18:22 +0530

    05/06 - 03

--- get_request.py
+++ get_request.py
@@ -4,11 +4,11 @@ def get_req(url, court_pk, date, hour):
 	# date entry example: 2017-09-22 (string) {yyyy-mm-dd}
 	# url example: 'https://abc.acb.com/' (string)
 	# court_pk - int
-	# hour example: 22 (10 PM) (int)
+	# hour example: 22 (10 PM) (string)
 	data = (requests.get(url + 'api/post_datetimevalue/')).json()
 	final_list = []
 	for item in data:
 		if item['court']==court_pk and item['date_time'][0:10].encode('ascii', 'replace')==date and item['date_time'][11:13].encode('ascii', 'replace')==hour:
-			final_list.append([item['date_time'].encode('ascii', 'replace'), item['value']])
+			final_list.append([item['date_time'].encode('ascii', 'replace')[11:19], item['value']])
 
 	return final_list
